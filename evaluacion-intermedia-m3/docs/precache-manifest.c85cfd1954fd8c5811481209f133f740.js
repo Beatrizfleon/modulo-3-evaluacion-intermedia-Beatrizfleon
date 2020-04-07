f32782bc549660498c21d3873ceb4372d969def1
Beatrizfleon <Beatrizfleon@gmail.com>
2020-04-07 09:01:44 +0200

    initial commit created rated react project

---
+++ evaluacion-intermedia-m3/docs/precache-manifest.c85cfd1954fd8c5811481209f133f740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18df2b1dbef1a42b10dd7f9d574dbb9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4679ed10904de508455c",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4175444063500bce9d6b",
+    "url": "./static/js/2.aa170963.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aa170963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4679ed10904de508455c",
+    "url": "./static/js/main.f210ac59.chunk.js"
+  },
+  {
+    "revision": "4d0122160da07903860a",
+    "url": "./static/js/runtime-main.05f6a8d7.js"
+  }
+]);
